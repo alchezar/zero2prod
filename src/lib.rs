6c3655e351078814b4836b12a69097fab5be5ffa
alchezar <kinder.wit@gmail.com>
2025-07-26 15:26:07 +0300

    First integration test

---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+﻿// IKinder
+
+//! lib.rs
+
+use actix_web::dev::Server;
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(server)
+}
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", name)
+}
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+// IKinder
+
+//! main.rs
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server.
+    let listener = std::net::TcpListener::bind("localhost:8000")?;
+    run(listener)?.await
 }
