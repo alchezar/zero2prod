7a825ab6708d9bac694bfab6d6b9e6497071a8ac
alchezar <kinder.wit@gmail.com>
2025-08-10 12:47:45 +0300

    Docker container

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 //! main.rs
 
 use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 use zero2prod::lib::configurations::get_configuration;
 use zero2prod::lib::startup::run;
@@ -15,11 +16,17 @@ async fn main() -> std::io::Result<()> {
 
     // Panic if we can't read configuration.
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres.");
+    // let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
+    // 	.expect("Failed to create Postgres connection pool.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string())
+        .expect("Failed to create Postgres connection pool.");
     // We have removed the hard-coded `8000` - it's now coming from our settings.
-    let address = format!("localhost:{}", configuration.application_port);
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
 
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server.
